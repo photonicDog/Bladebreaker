599486ea1912b7c304122b05d8a08f65707b0f02
jackie <photonicdog@gmail.com>
2021-09-20 20:37:35 +0100

    feat: Add Hitbox values component

---
+++ Assets/Scripts/Components/Hitbox.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Components
+{
+    public class Hitbox : Component
+    {
+        [Header("Damage")]
+        public byte Damage;
+
+        [Header("Knockback")]
+        public float HorizontalKnockback;
+        public float VerticalKnockback;
+
+        [Header("Hit Stun")]
+        public byte HitStunDuration;
+    }
+}
