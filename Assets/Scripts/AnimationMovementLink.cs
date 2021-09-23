667b50ff24daad84ee24fc00d35bbfd3689cdf71
nil <nillinvanillin@gmail.com>
2021-09-23 13:20:00 -0400

    added animation movement impulses for attacks
    improved the animator somewhat there's now a layer in the attack machine that distinguishes between various weapon movesets

---
+++ Assets/Scripts/AnimationMovementLink.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationMovementLink : MonoBehaviour
+{
+    //TODO: add facing check to the horizontal push method so it works left and right correctly
+    public EntityMovement targetScript;
+
+    public void PushEntityHorizontal(float impulse) {
+        targetScript.PushEntity(new Vector2(impulse, 0));
+    }
+
+    public void PushEntityVertical(float impulse) {
+        targetScript.PushEntity(new Vector2(0, impulse));
+    }
+}
