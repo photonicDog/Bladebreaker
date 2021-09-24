f87ea425801e43b431506069ca3f5d8754c8a3f8
rubiline <rubilineroundabout@gmail.com>
2021-09-24 04:49:39 -0400

    feat: cleared up bugs, added weapon durability, did weapon swapping, organized project files

--- Assets/Scripts/EntityAI.cs
+++ Assets/Scripts/EntityAI.cs
@@ -42,6 +42,8 @@ public class EntityAI : SerializedMonoBehaviour {
 
     private float entityRandomSpeed;
 
+    private float entityRandomSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
