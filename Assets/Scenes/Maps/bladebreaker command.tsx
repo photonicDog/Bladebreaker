a802f872a519d455493dceaf99e07a38c58c1447
rubiline <rubilineroundabout@gmail.com>
2021-09-27 20:09:49 -0400

    feat: added level

---
+++ Assets/Scenes/Maps/bladebreaker command.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="command tiles" tilewidth="16" tileheight="16" tilecount="49" columns="7">
+ <image source="command_tiles.png" width="112" height="112"/>
+</tileset>
---
+++ Assets/Scenes/Maps/ship_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="ship_tiles" tilewidth="8" tileheight="8" tilecount="280" columns="28">
+ <image source="C:/Users/JPlus/Downloads/ship_tiles.png" width="224" height="80"/>
+</tileset>
