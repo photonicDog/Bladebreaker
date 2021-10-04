3a060af07a56c9c5e2d7c49ad14361b8ba2c2fa0
jackie <photonicdog@gmail.com>
2021-10-04 17:28:43 +0100

    feat: Allow checkpoint to get camera bounds its stored in and set on respawn

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    public Collider2D CameraBounds;
+    private int BoundsLayer;
+
+    private void Awake()
+    {
+        BoundsLayer = LayerMask.GetMask("PlayerZone");
+    }
+
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player")) {
             int currentIndex = GameManager.Instance.checkpoints.FindIndex(
@@ -15,8 +23,24 @@ public class Checkpoint : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (CameraBounds == null)
+        {
+            Physics2D.queriesStartInColliders = true;
+            RaycastHit2D boundsCheck = Physics2D.Raycast(transform.position, Vector2.right, 2f, BoundsLayer);
+            if (boundsCheck.collider.gameObject.CompareTag("CameraBounds"))
+            {
+                CameraBounds = boundsCheck.collider.gameObject.GetComponent<BoxCollider2D>();
+                Physics2D.queriesStartInColliders = false;
+            }
+            Physics2D.queriesStartInColliders = false;
+        }
+    }
+
     public void TeleportToCheckpoint() {
         GameManager.Instance.player.transform.position = transform.position;
+        Camera.main.GetComponent<PlayerCamera>().CurrentArea = CameraBounds;
         GameManager.Instance.player.GetComponent<EntityMovement>().velocity = Vector2.zero;
     }
 }
