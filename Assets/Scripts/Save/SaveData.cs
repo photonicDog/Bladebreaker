3a89697595e4f665926735215e242f1c60de1026
Ruby Tartakovsky <rubilineroundabout@gmail.com>
2022-06-02 14:56:21 -0400

    feat!: save

---
+++ Assets/Scripts/Save/SaveData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace BladeBreaker.Save
+{
+    [System.Serializable]
+    public class SaveData
+    {
+        public List<int> Rankings;
+        public List<int> Scores;
+        public List<float> Times;
+        public List<int> Deaths;
+        public List<int> MaxCombos;
+        public List<int[]> Secrets;
+        public List<int> EnemiesDefeated;
+        public int LastClearedLevel;
+
+        private const int totalLevelCount = 3;
+        public SaveData()
+        {
+            Rankings = new List<int>();
+            Scores = new List<int>();
+            Times = new List<float>();
+            Deaths = new List<int>();
+            MaxCombos = new List<int>();
+            Secrets = new List<int[]>();
+            EnemiesDefeated = new List<int>();
+            LastClearedLevel = 0;
+
+            for(int i = 0; i < totalLevelCount; i++)
+            {
+                Rankings.Add(0);
+                Scores.Add(0);
+                Times.Add(0f);
+                Deaths.Add(0);
+                MaxCombos.Add(0);
+                EnemiesDefeated.Add(0);
+                Secrets.Add(new int[] { 0, 0, 0 });
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Save/SaveDataManager.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BladeBreaker.Save
+{
+    public class SaveDataManager : MonoBehaviour
+    {
+        private FileStream _saveFileStream;
+        private string _savePath;
+        private BinaryFormatter _bf;
+
+        public SaveData saveData;
+
+        private static SaveDataManager _instance;
+        public static SaveDataManager Instance { get { return _instance; } }
+
+        void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+            if (_instance != null && _instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                _instance = this;
+            }
+
+            _savePath = Application.persistentDataPath + "\\save.dat";
+            _bf = new BinaryFormatter();
+        }
+
+        public bool SaveData(int levelIndex, int ranking, int score, float time, int deaths, int maxCombo, int enemies, int[] secrets)
+        {
+            try
+            {
+                SaveData save = LoadData();
+
+                save.Rankings[levelIndex] = ranking;
+                save.Scores[levelIndex] = score;
+                save.Times[levelIndex] = time;
+                save.Deaths[levelIndex] = deaths;
+                save.MaxCombos[levelIndex] = maxCombo;
+                save.EnemiesDefeated[levelIndex] = enemies;
+                save.Secrets[levelIndex] = secrets;
+
+                OverwriteSave(save);
+                
+                saveData = save;
+                return true;
+            } catch (Exception ex)
+            {
+                Debug.Log($"Error saving data! {ex.Message}");
+                return false;
+            }
+        }
+
+        public SaveData LoadData()
+        {
+            SaveData save;
+
+            if (!File.Exists(_savePath))
+            {
+                save = CreateNewSave();
+            } else
+            {
+                _saveFileStream = File.Open(_savePath, FileMode.Open);
+                save = (SaveData)_bf.Deserialize(_saveFileStream);
+                _saveFileStream.Close();
+            }
+
+            saveData = save;
+            return save;
+        }
+
+        public SaveData CreateNewSave()
+        {
+            SaveData save = new SaveData();
+
+            OverwriteSave(save);
+
+            saveData = save;
+            return save;
+        }
+
+        private void OverwriteSave(SaveData save)
+        {
+            _saveFileStream = File.Create(_savePath);
+            _bf.Serialize(_saveFileStream, save);
+            _saveFileStream.Close();
+        }
+    }
+}
